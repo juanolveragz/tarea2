d767ac1b38db1e9a341710a7ce64c0a8c0769d31
juanolveragz <juan.olveragz@udlap.mx>
2015-09-02 17:58:22 -0500

    Update sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,29 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+Dividir(input);
+Cuadrados(arreglo, Cantidad);
+function Dividir(input)
+{
+	arreglo=input.split("\n");
+	Cantidad=arreglo[0];
+	return arreglo;
+	return Cantidad;
+}
+function Cuadrados(arreglo,Cantidad)
+{
+	for (i = 0; i < Cantidad; i++) {
+		temporal=arreglo[i+1].split(" ");
+		a=temporal[0];
+		b=temporal[1];
+		bandera=0;
+		for (a; a <= b; a++) {
+			num=a
+			sqrt = Math.sqrt(num);
+			if (sqrt%1==0){
+				bandera+=1;
+			}
+		};
+		console.log (bandera);
+	}
+}
